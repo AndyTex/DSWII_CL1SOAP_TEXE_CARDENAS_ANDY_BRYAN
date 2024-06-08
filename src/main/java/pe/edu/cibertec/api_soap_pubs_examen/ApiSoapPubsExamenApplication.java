ef9171d0ef50af0483a6a68b2606e437fa40aa78
WhiteNight07181 <andycnblue1234@gmail.com>
2024-06-08 14:18:08 -0500

    Creacion del Proyecto

---
+++ src/main/java/pe/edu/cibertec/api_soap_pubs_examen/ApiSoapPubsExamenApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.api_soap_pubs_examen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSoapPubsExamenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSoapPubsExamenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/api_soap_pubs_examen/ApiSoapPubsExamenApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.api_soap_pubs_examen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSoapPubsExamenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
